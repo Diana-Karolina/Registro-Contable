0feb51375ce5a683200596419ad9fa61a578b227
estefano97 <kaliestefano@gmail.com>
2023-01-08 20:08:22 -0500

    refactor code and add authorization

--- src/component/page/Login/Login.jsx
+++ src/component/page/Login/Login.jsx
@@ -1,17 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
 import LoginForm from "./LoginForm";
+import { useNavigate } from "react-router-dom";
 
 function Login() {
-  
+  const IsLogin = window.localStorage.getItem("token");
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (IsLogin) navigate("/");
+  }, [IsLogin]);
+
   return (
     <>
-    <nav>
-      <div className="navbar navbar-dark bg-dark">
+      <nav>
+        <div className="navbar navbar-dark bg-dark">
           <a className="navbar-brand tex-uppercase" href="/">
-            <h3 >Registro Contables</h3>
+            <h3>Registro Contables</h3>
           </a>
-      </div>
-    </nav>
+        </div>
+      </nav>
       <div className="container py-4">
         <div className="row">
           <h1>Login</h1>
